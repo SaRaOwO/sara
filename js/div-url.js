37da68484fd5ce7f0cc71d6b830326b266b9bb29
Evergaster <Evergaster@users.noreply.github.com>
2024-12-24 23:13:52 +0000

    deploy: c56c536c3284716dd179565079a33953e96bed27

---
+++ js/div-url.js
@@ -0,0 +1,12 @@
+const infoBoxes = document.querySelectorAll('.info-box');
+
+infoBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        // Obtiene el atributo "data-url" del elemento
+        const url = box.getAttribute('data-url');
+        if (url) {
+            // Redirige a la URL
+            window.open(url, '_blank'); // Abre en una nueva pestaña
+        }
+    });
+});
