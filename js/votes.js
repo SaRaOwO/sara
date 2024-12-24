6d7101207f84cd214c67ed469df9871d34523997
Evergaster <Evergaster@users.noreply.github.com>
2024-12-24 20:42:16 +0000

    deploy: b1a1c337c0667c0e725fa2b4093f9608c7042eda

--- js/votes.js
+++ js/votes.js
@@ -1,18 +1,12 @@
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg2ODk1NzI3NzYyMzgyNDM4NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNzM0ODk0OTU5fQ.iWUMFltXl6OKX9yPNY0mnhw1XZKy9QyIWIdEVe9sgq8';
-const BOT_ID = '868957277623824384';
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    async function getVotes() {
+    const BOT_ID = '868957277623824384';
 
-        const url = `https://top.gg/api/bots/${BOT_ID}`;
-    
-        const headers = {
-            'Authorization': API_KEY,
-        }
-        try{
-            const response = await axios.get(url, { headers });
-    
+    async function getVotes() {
+        const url = `https://top-gg-worker-morning-smoke-5bf3.garciaromeroeverardo38.workers.dev`;
+        
+        try {
+            const response = await axios.get(url);
+
             if (response.status === 200) {
                 const votos = response.data.monthlyPoints || 0;
                 document.getElementById('voteCount').innerText = votos;
@@ -20,13 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.error('Error:', response.status);
                 document.getElementById('voteCount').innerText = '?';
             }
-        }   catch (error) {
+        } catch (error) {
             console.error('Error al obtener votos:', error);
             document.getElementById('voteCount').innerText = '?';
         }
     }
+
     getVotes();
     setInterval(getVotes, 300000);
 });
-
- // 5 minutos
