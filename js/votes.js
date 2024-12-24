a2b6e4881243508a75025af6c1849a0eb43177a8
EverGaster <evergaterxd@gmail.com>
2024-12-24 13:48:12 -0600

    nose

--- js/votes.js
+++ js/votes.js
@@ -3,29 +3,32 @@ import axios from 'axios';
 const API_KEY = process.env.TOPGG_API;
 const BOT_ID = '868957277623824384';
 
-async function getVotes() {
 
-    const url = `https://top.gg/api/bots/${BOT_ID}`;
+document.addEventListener('DOMContentLoaded', function() {
+    async function getVotes() {
 
-    const headers = {
-        'Authorization': API_KEY,
-    }
-    try{
-        const response = await axios.get(url, { headers });
-
-        if (response.status === 200) {
-            const votos = response.data.monthlyPoints || 0;
-            document.getElementById('voteCount').innerText = votos;
-        } else {
-            console.error('Error:', response.status);
+        const url = `https://top.gg/api/bots/${BOT_ID}`;
+    
+        const headers = {
+            'Authorization': API_KEY,
+        }
+        try{
+            const response = await axios.get(url, { headers });
+    
+            if (response.status === 200) {
+                const votos = response.data.monthlyPoints || 0;
+                document.getElementById('voteCount').innerText = votos;
+            } else {
+                console.error('Error:', response.status);
+                document.getElementById('voteCount').innerText = '?';
+            }
+        }   catch (error) {
+            console.error('Error al obtener votos:', error);
             document.getElementById('voteCount').innerText = '?';
         }
-    }   catch (error) {
-        console.error('Error al obtener votos:', error);
-        document.getElementById('voteCount').innerText = '?';
     }
-}
+    getVotes();
+    setInterval(getVotes, 300000);
+});
 
-document.addEventListener('DOMContentLoaded', getVotes);
-
-setInterval(getVotes, 300000); // 5 minutos
+ // 5 minutos
