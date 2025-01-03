97b8a5851100c5a42bc66ba63e28ed6c84c761e2
EverGaster <evergaterxd@gmail.com>
2025-01-02 20:57:58 -0600

    refactor: actualizar selectores en sliderbar.js y agregar nuevo archivo redireccion.html

--- js/nsfw.js
+++ js/nsfw.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const uwuMode = urlParams.get('nsfw') === 'true';
-
-    if (uwuMode) {
-      document.body.classList.add('nsfw-mode');
-      
-      const sectionImages = document.querySelectorAll('.section-image');
-      sectionImages.forEach((img, index) => {
-        img.style.backgroundImage = `url('/img/icons/uwu-section-${index + 1}.webp')`;
-      });
+  const urlParams = new URLSearchParams(window.location.search);
+  const uwuMode = urlParams.get('nsfw') === 'true';
+  
+  if (uwuMode) {
+    // Añadir clase al body
+    document.body.classList.add('nsfw-mode');
+    
+    // Cambiar imagen del hero
+    const hero = document.getElementById('hero');
+    if (hero) {
+      hero.style.backgroundImage = "url('/img/modeusnsfw.svg')";
     }
-  });
+    
+    // Cambiar imágenes de las secciones
+    const sectionImages = document.querySelectorAll('.section-image');
+    sectionImages.forEach((img, index) => {
+      img.style.backgroundImage = `url('/img/icons/uwu-section-${index + 1}.webp')`;
+    });
+  }
+});
